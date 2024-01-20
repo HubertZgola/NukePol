aa40b7830b48ca0f5a8661d06fe295c3e754113d
Hubert <hubert.zgola@gmail.com>
2024-01-20 13:47:45 +0100

    Added few warheads

--- src/components/BasicSettings.js
+++ src/components/BasicSettings.js
@@ -58,14 +58,15 @@ const BasicSettings = ({city, handleWarheadChange, updateBasicSettings, setCity,
       setSelectedWarhead(e.target.value); // Aktualizacja lokalnego stanu
       handleWarheadChange(e.target.value); // Aktualizacja globalnego stanu w App.js
     }}>
-        <option value="20t">„Davy Crockett” – USA (20 t)</option>
-        <option value="15kt">„Little Boy” – bomba w Hiroszimie (15 kt)</option>
-        <option value="100Mt">„Car Bomba” - największa zaprojektowana bomba ZSRR (100 Mt)</option>
+        <option value="20t">„Davy Crockett” – Najmniejsza wyprodukowana bomba w USA (20 t)</option>
+        <option value="300t">„B-61 Mod 3” – Najmniejsza obecnie posiadana bomba w arsenele USA (300 t)</option>
+        <option value="15kt">„Little Boy” – Bomba zdetonowana w Hiroszimie (15 kt)</option>
+        <option value="100kt">„W-76” – Posiadana w arsenele SLBM w USA i w Wielkiej Brytani (100 kt)</option>
+        <option value="300kt">„TN 80/81” – Największa bomba posiadana w arsenale Francji (300kt)</option>
+        <option value="800kt">„Topol (SS-25)” – Bomba w obecnym arsenale Rosyjskim (800kt)</option>
+        <option value="5Mt">„Dong Feng-5” – Rakietowy pocisk balistyczny międzykontynentalnego zasięgu z obecnego arsenału Chin (5 Mt)</option>
+        <option value="100Mt">„Car Bomba” – Największa zaprojektowana bomba ZSRR (100 Mt)</option>
       </select>
-      <label>
-        <h3>Wyświetl ilość ofiar</h3>
-        <input type="checkbox" name="displayCasualties"/>
-      </label>
       <button className="danger" onClick={handleDetonateClick}>Detonuj</button>
     </div>
   );
--- src/ulilities/warheadRadii.js
+++ src/ulilities/warheadRadii.js
@@ -1,4 +1,3 @@
-
 const warheadRadii = {
     SurfaceExplosion: {
         '20t': {
@@ -19,6 +18,24 @@ const warheadRadii = {
           Trzeciego: 105,
           fireball: 7
       },
+      '300t': {
+        overpressure3000psi: 2,
+        overpressure200psi: 15,
+        overpressure20psi: 42,
+        overpressure5psi: 88,
+        overpressure1psi: 226, 
+
+        ionizing20Sv: 207,
+        ionizing6Sv: 289,
+        ionizing1Sv: 443,
+        ionizing01Sv: 698,
+        ionizing0025Sv: 883,
+
+        Pierwszego: 181,
+        Drugiego: 125,
+        Trzeciego: 105,
+        fireball: 7
+    },
       '15kt': {
           overpressure3000psi: 60,
           overpressure200psi: 138,
@@ -37,6 +54,78 @@ const warheadRadii = {
           Trzeciego: 1285,
           fireball: 185
       },
+      '100kt': {
+        overpressure3000psi: 0.1,
+        overpressure200psi: 143,
+        overpressure20psi: 400,
+        overpressure5psi: 905,
+        overpressure1psi: 2350,
+
+        ionizing20Sv: 753,
+        ionizing6Sv: 916,
+        ionizing1Sv: 1174,
+        ionizing01Sv: 1535,
+        ionizing0025Sv: 1789,
+
+        Pierwszego: 2590,
+        Drugiego: 1860,
+        Trzeciego: 1523,
+        fireball: 144
+    },
+    '300kt': {
+        overpressure3000psi: 0.1,
+        overpressure200psi: 143,
+        overpressure20psi: 400,
+        overpressure5psi: 905,
+        overpressure1psi: 2350,
+
+        ionizing20Sv: 753,
+        ionizing6Sv: 916,
+        ionizing1Sv: 1174,
+        ionizing01Sv: 1535,
+        ionizing0025Sv: 1789,
+
+        Pierwszego: 2590,
+        Drugiego: 1860,
+        Trzeciego: 1523,
+        fireball: 144
+    },
+      '800kt': {
+        overpressure3000psi: 60,
+        overpressure200psi: 138,
+        overpressure20psi: 380,
+        overpressure5psi: 800,
+        overpressure1psi: 2050,
+
+        ionizing20Sv: 761,
+        ionizing6Sv: 922,
+        ionizing1Sv: 1180,
+        ionizing01Sv: 1540,
+        ionizing0025Sv: 1794,
+
+        Pierwszego: 2190,
+        Drugiego: 1570,
+        Trzeciego: 1285,
+        fireball: 185
+    },
+    '5Mt': {
+        overpressure3000psi: 1050,
+        overpressure200psi: 2600,
+        overpressure20psi: 7150,
+        overpressure5psi: 15050,
+        overpressure1psi: 38750,
+        
+        ionizing20Sv: 4757,
+        ionizing6Sv: 4919,
+        ionizing1Sv: 5146,
+        ionizing01Sv: 5544,
+        ionizing0025Sv: 5944,
+
+        Pierwszego: 84550,
+        Drugiego: 67329,
+        Trzeciego: 54680,
+        fireball: 6225
+    },
       '100Mt': {
         overpressure3000psi: 1088,
         overpressure200psi: 2641,
@@ -75,6 +164,24 @@ const warheadRadii = {
           Trzeciego: 65,
           fireball: 10
       },
+      '300t': {
+        overpressure3000psi: 2,
+        overpressure200psi: 15,
+        overpressure20psi: 42,
+        overpressure5psi: 88,
+        overpressure1psi: 226, 
+
+        ionizing20Sv: 207,
+        ionizing6Sv: 289,
+        ionizing1Sv: 443,
+        ionizing01Sv: 698,
+        ionizing0025Sv: 883,
+
+        Pierwszego: 181,
+        Drugiego: 125,
+        Trzeciego: 105,
+        fireball: 7
+    },
       '15kt': {
           overpressure3000psi: 0.1,
           overpressure200psi: 143,
@@ -93,6 +200,78 @@ const warheadRadii = {
           Trzeciego: 1523,
           fireball: 144
       },
+      '100kt': {
+        overpressure3000psi: 0.1,
+        overpressure200psi: 143,
+        overpressure20psi: 400,
+        overpressure5psi: 905,
+        overpressure1psi: 2350,
+
+        ionizing20Sv: 753,
+        ionizing6Sv: 916,
+        ionizing1Sv: 1174,
+        ionizing01Sv: 1535,
+        ionizing0025Sv: 1789,
+
+        Pierwszego: 2590,
+        Drugiego: 1860,
+        Trzeciego: 1523,
+        fireball: 144
+    },
+    '300kt': {
+        overpressure3000psi: 0.1,
+        overpressure200psi: 143,
+        overpressure20psi: 400,
+        overpressure5psi: 905,
+        overpressure1psi: 2350,
+
+        ionizing20Sv: 753,
+        ionizing6Sv: 916,
+        ionizing1Sv: 1174,
+        ionizing01Sv: 1535,
+        ionizing0025Sv: 1789,
+
+        Pierwszego: 2590,
+        Drugiego: 1860,
+        Trzeciego: 1523,
+        fireball: 144
+    },
+      '800kt': {
+        overpressure3000psi: 60,
+        overpressure200psi: 138,
+        overpressure20psi: 380,
+        overpressure5psi: 800,
+        overpressure1psi: 2050,
+
+        ionizing20Sv: 761,
+        ionizing6Sv: 922,
+        ionizing1Sv: 1180,
+        ionizing01Sv: 1540,
+        ionizing0025Sv: 1794,
+
+        Pierwszego: 2190,
+        Drugiego: 1570,
+        Trzeciego: 1285,
+        fireball: 185
+    },
+    '5Mt': {
+        overpressure3000psi: 1050,
+        overpressure200psi: 2600,
+        overpressure20psi: 7150,
+        overpressure5psi: 15050,
+        overpressure1psi: 38750,
+        
+        ionizing20Sv: 4757,
+        ionizing6Sv: 4919,
+        ionizing1Sv: 5146,
+        ionizing01Sv: 5544,
+        ionizing0025Sv: 5944,
+
+        Pierwszego: 84550,
+        Drugiego: 67329,
+        Trzeciego: 54680,
+        fireball: 6225
+    },
       '100Mt': {
           overpressure3000psi: 1050,
           overpressure200psi: 2600,
@@ -113,4 +292,3 @@ const warheadRadii = {
       }
   }};
 export default warheadRadii;
-    
